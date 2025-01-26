b6fac8b95af1831e44930f6d49d10618747de78f
Ayrton-Taede Tromp <ayrton@bebore.com>
2025-01-26 18:41:29 +0100

    Removed deprecated syntax

--- init.lua
+++ init.lua
@@ -634,10 +634,10 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        typescript = { { 'prettierd', 'prettier' }, stop_after_first = true },
-        typescriptreact = { { 'prettierd', 'prettier' }, stop_after_first = true },
-        javascript = { { 'prettierd', 'prettier' }, stop_after_first = true },
-        javascriptreact = { { 'prettierd', 'prettier' }, stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
       },
